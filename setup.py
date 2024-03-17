a7768be81cfc8ea27600ca461db133c4de5b379c
Andres <garcicon45@gmail.com>
2024-03-17 15:39:27 -0300

    1.1.2

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pdf2vectors',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     author='Andres Garcia',
     author_email='garcicon45@gmail.com',
     description='A package to interact with vectors DB',
-    long_description='A package to interact with vectors DB',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ruteru/pdf2vectors',
     classifiers=[
